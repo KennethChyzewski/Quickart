42e997b118b1c3131c328e8c46e616c7e1668131
ganweizo <Gan>
2020-10-27 17:50:57 -0400

    Waiting on Routing

--- quickart-main/src/react-components/StickyBar.js
+++ quickart-main/src/react-components/StickyBar.js
@@ -4,24 +4,39 @@ class StickyBar extends React.Component {
     render(){
             <div class="sideBarContainer">  
                 <h4>Search For Items</h4>
-                <input type="text" placeholder="Search.."></input>
+                <input type="text" class="searchbar" placeholder="Search.."></input>
                 
                 <h4>Food Tags</h4>
                 <div class="filter">
                     <div id="Tag-Content" class="tagContent">
-                        <a href="Tag">+Tag</a>
+                    <div class="tagCheckbox">
+                            <input type="checkbox" id="Food1" name="Food1">
+                                <label for="Food1">Apples</label>
+                                <br></br>
+                            </input>
+                        </div>
                     </div>
                 </div>
                 <h4>Location</h4>
                 <div class="filter">
                     <div id="Tag-Content" class="tagContent">
-                        <a href="Tag">+Tag</a>
+                    <div class="tagCheckbox">
+                            <input type="checkbox" id="Location1" name="Location1">
+                                <label for="Location1">Hell</label>
+                                <br></br>
+                            </input>
+                        </div>
                     </div>
                 </div>
                 <h4>Bid Price</h4>
                 <div class="filter">
                     <div id="Tag-Content" class="tagContent">
-                        <a href="Tag">+Tag</a>
+                        <div class="tagCheckbox">
+                            <input type="checkbox" id="Price1" name="Price1">
+                                <label for="Price">0$ - 100$</label>
+                                <br></br>
+                            </input>
+                        </div>
                     </div>
                 </div>
             </div>  
