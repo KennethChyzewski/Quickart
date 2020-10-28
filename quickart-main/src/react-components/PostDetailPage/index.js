70204d08b6e1e69ea277deabb1f6ed04f4139471
ganweizo <Gan>
2020-10-27 23:52:56 -0400

    CSS makes life hard

--- quickart-main/src/react-components/PostDetailPage/index.js
+++ quickart-main/src/react-components/PostDetailPage/index.js
@@ -1,4 +1,8 @@
 import React from 'react'
+import Button from '@material-ui/core/Button';
+import AddIcon from '@material-ui/icons/Add';
+
+import "./styles.css";
 
 class DetailedPost extends React.Component {
     render(){
@@ -12,8 +16,9 @@ class DetailedPost extends React.Component {
                 </div>
 
                 <form class="form">
-                    <textarea class="inputGroup" placeholder="Bid Value in (CAD) "></textarea> <br></br>
-                    <textarea class="inputGroup" placeholder="Your message here"></textarea> <br></br>
+                    <textarea class="inputGroup" placeholder="Bid Value in (CAD) "></textarea> 
+                    <textarea class="inputGroup" placeholder="Your message here"></textarea> 
+                    <br></br>
                     <input type="submit" value="Submit" class="btn btn" />
                 </form>
             </div>
@@ -21,41 +26,85 @@ class DetailedPost extends React.Component {
             {/* Information on product */}
             <div class="posts">
                 <div class="infomationColour" >
-                    <h4>Product Info/Description</h4>
-                    <p class="paragraphColour">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                    </p>
+                    <div>
+                        <h4>Product Info/Description</h4>
+                        <p class="paragraphColour">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        </p>
 
-                    <h4> PickUp/Delivery Options </h4>
-                    <p class="paragraphColour">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                    </p>
+                        <h4> PickUp/Delivery Options </h4>
+                        <p class="paragraphColour">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        </p>
+                    </div>
+
                     <h4> Pictures </h4>
-                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f4/Honeycrisp.jpg/1200px-Honeycrisp.jpg" alt="Aples" width="500" height="600"></img>
-                    {/*  Add the Modal stuff here so it pops out */}
+                    <div class="picContainer" >
+                        <img class="picContent" src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f4/Honeycrisp.jpg/1200px-Honeycrisp.jpg" alt="Aples" width="500" height="600"></img>
+                        {/*  Add the Modal stuff here so it pops out */}
+                    </div>
                 </div>
             </div>
             
             {/*Information on seller */ }
-            <div >
-                <div>
+            <div class="">
+                <div class="picLeft" >
                     <a href="profilesPage.html">
-                        <img class="profilePicCircle" src="https://slm-assets.secondlife.com/assets/10261331/view_large/y_c9278081.jpg?1410113241" alt="Applemaker"/>
+                        <img class="picLeft" src="https://slm-assets.secondlife.com/assets/10261331/view_large/y_c9278081.jpg?1410113241" alt="Applemaker"/>
                     </a>
                 </div>
-
                 <div class="informationColour">
-                    <h4>Johnson Smith</h4>
-                    <p class = "paragraphColour">
-                        Insert quick blob about himself
-                    </p>
-                    <h5> Tags </h5>
-                    <ul class='tagBar'>
-                        <a class="tagOption" href=""> + Tag </a>
-                        <a class="tagOption" href=""> + Tag </a>
-                        <a class="tagOption" href=""> + Tag </a>
-                        <a class="tagOption" href=""> + Tag </a>
+                    <div>
+                        <a href="profilesPage.html">
+                            <h3>Johnson Smith</h3>
+                        </a>
+                        <p class = "paragraphColour profileDesc">
+                            Insert quick blob about himself
+                        </p>
+                    </div>
+
+                    <div class='tagBar'>
+                    <h4> Tags </h4>
+                    <ul class="tagbaritems">
+                        <Button
+                            size="small" 
+                            variant="outlined" 
+                            href="" 
+                            startIcon={<AddIcon/>}
+                            class="tagOption"
+                            >
+                            BlackMarket
+                        </Button>
+                        <Button
+                            size = "small" 
+                            variant="outlined" 
+                            href="" 
+                            startIcon={<AddIcon/>}
+                            class="tagOption produce"
+                            >
+                            Apples 
+                        </Button>
+                        <Button
+                            size = "small" 
+                            variant="outlined" 
+                            href="" 
+                            startIcon={<AddIcon/>}
+                            class="tagOption grain"
+                            >
+                            Rice 
+                        </Button>
+                        <Button
+                            size = "small" 
+                            variant="outlined" 
+                            href="" 
+                            startIcon={<AddIcon/>}
+                            class="tagOption meat"
+                            >
+                            Cows
+                        </Button>
                     </ul>
+                    </div>
+                    
                 </div>
             </div>
         </section>
