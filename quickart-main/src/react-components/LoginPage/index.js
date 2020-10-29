561e934d33d6f26d4f310dc8fc0fe90be4d70544
Zi Shuai Gao <keving.gao@mail.utoronto.ca>
2020-10-29 18:29:43 -0400

    Added form validation and getting onChangeEvents for the Login and Register forms

--- quickart-main/src/react-components/LoginPage/index.js
+++ quickart-main/src/react-components/LoginPage/index.js
@@ -5,30 +5,60 @@ import './styles.css';
 
 /* Component for the Login Page */
 class LoginPage extends React.Component {
+  state = {
+    username: '',
+    userPassword: '',
+  };
+
+  onChangeEvent = e => {
+    this.setState({
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  onSubmitEvent = e => {
+    e.preventDefault();
+    //Checks if any of the login fields are empty
+    if (!this.state.username) {
+      console.log('A username is required.');
+    }
+    if (!this.state.userPassword) {
+      console.log('A password is required.');
+    } else {
+      //Makes a call to the back-end to verify the user credentials
+      console.log(this.state);
+      console.log('User has successfully been logged in.');
+    }
+  };
+
   render() {
     return (
       <section className='mainBackground'>
         <div className='containerForm'>
           <h1 className='textDefaultColor'>Sign in</h1>
-          <form>
+          <form className='form' onSubmit={this.onSubmitEvent}>
             <div className='form-group'>
               <input
                 className='inputGroup'
+                id='username'
                 type='text'
                 placeholder='Username'
+                onChange={this.onChangeEvent}
               />
             </div>
             <div className='form-group'>
               <input
                 className='inputGroup'
+                id='userPassword'
                 type='password'
                 placeholder='Password'
+                onChange={this.onChangeEvent}
               />
             </div>
 
-            <Link to='/posts' type='submit' className='btn btnDefault'>
+            <button type='submit' value='Login' className='btn btnDefault'>
               Login
-            </Link>
+            </button>
           </form>
           <p>
             Don't have an Account? <Link to='/register'>Register Here</Link>
