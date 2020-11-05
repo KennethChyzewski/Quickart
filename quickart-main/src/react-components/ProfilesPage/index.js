fa013d3b7b4a5d4690bd81e7f9035184a566f03b
ganweizo <Gan>
2020-11-04 20:20:25 -0500

    Admin Profile Page complete

--- quickart-main/src/react-components/ProfilesPage/index.js
+++ quickart-main/src/react-components/ProfilesPage/index.js
@@ -1,12 +1,42 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import userPicture from '../../images/defaultUserPicture.jpg';
-
+import store from '../../store';
 import './styles.css';
 
 /* Component for the User Profile Page */
 class ProfilesPage extends React.Component {
   render() {
+    const state = store.getState()
+    let userType = state['loginReducer']['payload']["accType"]
+    let isAdmin = userType === "admin"
+
+    const postings = (
+      <div className='profile-posts'>
+        <div className='profile-post-title'>
+          <h2 className='textDefaultColor'>Posts</h2>
+        </div>
+        <div className='backgroundWhite'>
+          <div>
+            <h4>
+              <Link to='/login'>Title</Link>
+            </h4>
+            <h6>Date</h6>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                do eiusmod tempor incididunt ut labore et dolore magna
+                aliqua.
+            </p>
+          </div>
+        </div>
+      </div>
+    )
+    const userReports = (
+      <Link to='/userReports' className='btn btnDefault'>
+        User Reports
+      </Link>
+    )
+
     return (
       <section className='mainBackground'>
         <div className='containerProfile'>
@@ -17,6 +47,12 @@ class ProfilesPage extends React.Component {
               <p>Toronto, ON</p>
             </div>
             <div className='profile-about backgroundGrey '>
+            <div className='buttons smallMargin'>
+              <Link to='/editProfile' className='btn btnDefault'>
+                Edit Profile
+              </Link>
+              {isAdmin ? userReports: ""}
+            </div>
               <h2 className='textDefaultColor'>Biography</h2>
               <p>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
@@ -28,30 +64,7 @@ class ProfilesPage extends React.Component {
                 eiusmod tempor incididunt ut labore et dolore magna aliqua.
               </p>
             </div>
-            <div className='profile-posts'>
-              <div className='profile-post-title'>
-                <h2 className='textDefaultColor'>Posts</h2>
-              </div>
-              <div className='backgroundWhite'>
-                <div>
-                  <h4>
-                    <Link to='/login'>Title</Link>
-                  </h4>
-                  <h6>Date</h6>
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua.
-                  </p>
-                </div>
-              </div>
-            </div>
-
-            <div className='buttons smallMargin'>
-              <Link to='/editProfile' className='btn btnDefault'>
-                Edit Profile
-              </Link>
-            </div>
+            {isAdmin ? "" : postings}
           </div>
         </div>
       </section>
