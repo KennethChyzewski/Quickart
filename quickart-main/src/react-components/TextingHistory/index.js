47cb131ca0f494e88d4cf6eb163ebb804f182b42
Wafiqah Raisa <wafiqah.raisa@mail.utoronto.ca>
2020-11-08 01:42:32 -0500

    implemented vanish mode

--- quickart-main/src/react-components/TextingHistory/index.js
+++ quickart-main/src/react-components/TextingHistory/index.js
@@ -2,7 +2,9 @@ import React from "react"
 import "./styles.css"
 
 import vanishModeImage from "./../../images/vanish-mode-image.png"
+
 import Message from "./../Message"
+import VanishMode from "./../VanishMode"
 
 
 var today = new Date();
@@ -28,24 +30,45 @@ class TextingHistory extends React.Component {
 			],
 			sentMessage: null,
 			vanishMode: false,
+			vanishModeHistory: []
 		}
 	}
 
-	activateVanishMode() {
-   		this.setState({
-			vanishMode: true
-		})
+	handleVanishMode() {
+		if (this.state.vanishMode) {
+			this.setState({
+				vanishMode: false,
+				vanishModeHistory: [] // erase history because it's vanish mode
+			})
+		}
+		else {
+   			this.setState({
+   				vanishMode: true
+   			})
+   		}
 
 	}
 
 	handleChange(event) {
-		if(event.key === 'Enter') { 
-			if (event.target.value.replace(/\s/g, '').length) { // catches strings that are just whitespace or empty, we don't want to render these messages.
-				this.setState({
-					message: true,
-					history: this.state.history.concat([[event.target.value, today, true, false]]),
-					sentMessage: ""
-				})
+		if(event.key === 'Enter') {
+			//if (event.target.value.replace(/\s/g, '').length)
+			//if (event.target.value || event.target.value.length > 0)
+			console.log("mesage: ", event.target.value)
+			if (!(event.target.value.trim() === "")) { // catches strings that are just whitespace or empty, we don't want to render these messages.
+				if (this.state.vanishMode) {
+					this.setState({
+						message: true,
+						vanishModeHistory: this.state.vanishModeHistory.concat([[event.target.value, today, true, false]]),
+						sentMessage: ""
+					})	
+				}
+				else {
+					this.setState({
+						message: true,
+						history: this.state.history.concat([[event.target.value, today, true, false]]),
+						sentMessage: ""
+					})
+				}
 			}
 			this.mainInput.value = ""
     	}
@@ -67,6 +90,7 @@ class TextingHistory extends React.Component {
 
 	render() {
 		const messageHistory = this.state.history.map((item,index) =>  <Message key={index} messageText={item[0]} date={item[1]} location={item[2]} isImage={item[3]}/>)
+		const vanishModeHistory = this.state.vanishModeHistory.map((item,index) =>  <Message key={index} messageText={item[0]} date={item[1]} location={item[2]} isImage={item[3]}/>)
 		return (
 			<div className="messageBox">
 				<div className="messageHeader">
@@ -80,7 +104,7 @@ class TextingHistory extends React.Component {
 				<div className="chatHistoryContainer">
 					<div className="chatHistoryBox">
 						<div>
-							{messageHistory}
+							{this.state.vanishMode ? <VanishMode vanishModeHistory={vanishModeHistory}/> : messageHistory}
 						</div>
 						<div style={{ float:"left", clear: "both" }}
              				ref={(el) => { this.messagesEnd = el; }}>
@@ -90,13 +114,8 @@ class TextingHistory extends React.Component {
 				<div className="textInputContainer">
 					<input ref={(ref) => this.mainInput= ref} placeholder="Message..." type="text" className="textInputArea" onKeyDown={this.handleChange.bind(this)}/>
 					<div className="buttonArea">
-						<div className="vanishButton"> <img alt="profile pic" src={vanishModeImage} className="sendMessageImage" onClick={this.activateVanishMode.bind(this)}/> </div>	
+						<div className="vanishButton"> <img alt="profile pic" src={vanishModeImage} className="sendMessageImage" onClick={this.handleVanishMode.bind(this)}/> </div>	
 					</div>
-					<div className="buttonArea">
-			            
-						
-					</div>
-
 				</div>
 			</div>
 		)
