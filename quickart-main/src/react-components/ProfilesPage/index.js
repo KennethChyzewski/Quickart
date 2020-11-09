859768fb3e60ce8e19226470c771cb2af4571921
Zi Shuai Gao <keving.gao@mail.utoronto.ca>
2020-11-08 22:40:08 -0500

    Changed some color scheme

--- quickart-main/src/react-components/ProfilesPage/index.js
+++ quickart-main/src/react-components/ProfilesPage/index.js
@@ -7,75 +7,74 @@ import './styles.css';
 /* Component for the User Profile Page */
 class ProfilesPage extends React.Component {
   render() {
-    const state = store.getState()
-    let userType = state['loginState']['payload']["accType"]
-    let isAdmin = userType === "admin"
+    const state = store.getState();
+    let userType = state['loginState']['payload']['accType'];
+    let isAdmin = userType === 'admin';
 
     const postings = (
       <div className='profile-posts'>
         <div className='profile-post-title'>
-          <h2 className='textDefaultColor'>Posts</h2>
+          <h2 className='textDefaultColor addSomeMargin'>Posts</h2>
         </div>
         <div className='backgroundWhite'>
           <div>
             <h4>
-              <Link to='/posts'>Title</Link>
+              <Link className='addSomeMargin' to='/posts'>
+                Title
+              </Link>
             </h4>
-            <h6>Date</h6>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-              do eiusmod tempor incididunt ut labore et dolore magna
-              aliqua.
+            <h6 className='addSomeMargin'>Date</h6>
+            <p className='addSomeMargin'>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
             </p>
           </div>
         </div>
       </div>
-    )
-    
+    );
+
     const niche = (
       <div>
-        <h2 className='textDefaultColor'>Niche</h2>
-        <p>
+        <h2 className='textDefaultColor addSomeMargin'>Niche</h2>
+        <p className='addSomeMargin'>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua.
         </p>
       </div>
-    )
+    );
     const userReports = (
       <Link to='/userReports' className='btn btnDefault'>
         User Reports
       </Link>
-    )
+    );
 
     return (
       <section className='mainBackground-profile'>
         <div className='containerProfile'>
           <div className='profileArea'>
-          <div className='buttons smallMargin'>
-                <Link to='/editProfile' className='btn btnDefault'>
-                  Edit Profile
-                </Link>
-                {isAdmin ? userReports: ""}
+            <div className='buttons smallMargin'>
+              <Link to='/editProfile' className='btn btnDefault'>
+                Edit Profile
+              </Link>
+              {isAdmin ? userReports : ''}
             </div>
-            
+
             <div className='profile-top backgroundDefault'>
               <img className='profileImg' src={userPicture} alt='' />
               <h1>Johnson Smith</h1>
               <p>Toronto, ON</p>
             </div>
 
-            
-
             <div className='profile-about backgroundGrey '>
-              <h2 className='textDefaultColor'>Biography</h2>
-              <p>
+              <h2 className='textDefaultColor addSomeMargin'>Biography</h2>
+              <p className='addSomeMargin'>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
                 eiusmod tempor incididunt ut labore et dolore magna aliqua.
               </p>
-              {isAdmin ? "" : niche}
+              {isAdmin ? '' : niche}
             </div>
-            {isAdmin ? "" : postings}
-        </div>
+            {isAdmin ? '' : postings}
+          </div>
         </div>
       </section>
     );
