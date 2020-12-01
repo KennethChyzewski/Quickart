c9eea5cbbfe5679631483e70cf646f7cd8176bbd
Zi Shuai Gao <keving.gao@mail.utoronto.ca>
2020-11-30 22:18:00 -0500

    Fixed accType Error on welcome Page

--- quickart-main/src/react-components/NavigationBar/index.js
+++ quickart-main/src/react-components/NavigationBar/index.js
@@ -9,81 +9,83 @@ import './styles.css';
 /* Component for the NavigationBar */
 class NavigationBar extends React.Component {
   render() {
-    const state = store.getState()
-    let login = state['loginState']
-    let isLoggedIn = (Object.keys(login).length !== 0) ? true : false;
-  
+    const state = store.getState();
+    let login = state['loginState'];
+    let isLoggedIn = Object.keys(login).length !== 0 ? true : false;
+
     const notLoggedInNavBar = (
       <ul>
-            <li id='navAbout'>
-              <Link to='/about'>About</Link>
-            </li> 
-            <li id='navRegister'>
-              <Link to='/register'>Sign Up</Link>
-            </li>
-            <li id='navLogin'>
-              <Link to='/login'>Login</Link>
-            </li>
+        <li id='navAbout'>
+          <Link to='/about'>About</Link>
+        </li>
+        <li id='navRegister'>
+          <Link to='/register'>Sign Up</Link>
+        </li>
+        <li id='navLogin'>
+          <Link to='/login'>Login</Link>
+        </li>
       </ul>
     );
 
     const loggedInNavBar = (
       <ul>
-            <li id='navPosts'>
-              <Link to='/posts'>Posts</Link>
-            </li>
-            <li id='navProfile'>
-              <Link to='/profile'>Profile</Link>
-            </li>
-            <li id='navMessages'>
-              <Link to='/messages'>Messages</Link>
-            </li>
-            <li id='navLogout'>
-              <Link onClick={this.props.logout} to='/'>Log Out</Link>
-            </li>
+        <li id='navPosts'>
+          <Link to='/posts'>Posts</Link>
+        </li>
+        <li id='navProfile'>
+          <Link to='/profile'>Profile</Link>
+        </li>
+        <li id='navMessages'>
+          <Link to='/messages'>Messages</Link>
+        </li>
+        <li id='navLogout'>
+          <Link onClick={this.props.logout} to='/'>
+            Log Out
+          </Link>
+        </li>
       </ul>
     );
 
-
     const adminHome = (
       <Link to='/userReports'>
-          <i className='fas fa-shopping-cart'></i> Quickart
+        <i className='fas fa-shopping-cart'></i> Quickart
       </Link>
-    )
+    );
     const userHome = (
       <Link to='/posts'>
-          <i className='fas fa-shopping-cart'></i> Quickart
+        <i className='fas fa-shopping-cart'></i> Quickart
       </Link>
-    )
+    );
     const loginHome = (
       <Link to='/'>
-          <i className='fas fa-shopping-cart'></i> Quickart
+        <i className='fas fa-shopping-cart'></i> Quickart
       </Link>
-    )
-    
-    function redirector(){
-      if(isLoggedIn){
-        let userType = state['loginState']['payload']['accType'];
-        let isAdmin = userType === "admin";
-        return isAdmin ? adminHome : userHome
-      }else{
-        return loginHome
+    );
+
+    function redirector() {
+      if (state['loginState']['isAuthenticated'] != null) {
+        console.log(state['loginState']['isAuthenticated']);
+        return userHome;
+      } else {
+        return loginHome;
       }
     }
-    
+
     return (
       <nav className='navbar backgroundBlue'>
-        <h1>
-          {redirector()}
-        </h1>
-        { isLoggedIn ? <div>{ loggedInNavBar }</div> : <div>{ notLoggedInNavBar }</div> }
+        <h1>{redirector()}</h1>
+        {isLoggedIn ? (
+          <div>{loggedInNavBar}</div>
+        ) : (
+          <div>{notLoggedInNavBar}</div>
+        )}
       </nav>
     );
   }
 }
 
 const mapStateToProps = state => ({
-  login: state.loginState
-})
+  login: state.loginState,
+});
 
 export default connect(mapStateToProps, { logout })(NavigationBar);
